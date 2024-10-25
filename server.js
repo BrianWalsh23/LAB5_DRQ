ad66d981d2bbb5b4d70ce838da93cb42d5d94b2d
Brian Walsh <G00424410@atu.ie>
2024-10-25 08:41:24 +0000

    Exercise 3 Complete

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Listens for a get Method and / = URL path. Once both are satisified,
+// executes arrow function. 
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
 
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Listening on port 3000
+// Always has to be at the bottom
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
